0f643b2311e69d0c22f7c4809ea07e98203f4de8
vladislavbagnyuk <vladislav.bagnyuk@gmail.com>
2020-09-26 01:07:45 +0200

    Choosing types of password

--- src/App.js
+++ src/App.js
@@ -22,11 +22,98 @@ class App extends Component {
     this.generatePassword();
   }
 
-  generatePassword = () => {
-    const password = cryptoRandomString({
-      length: 15,
-      type: "ascii-printable",
-    });
+  generatePassword = (checkboxes = this.state.checkboxes) => {
+    let password = null;
+    let args = null;
+
+    // Only letters
+    if (
+      checkboxes[0].checked &&
+      !checkboxes[1].checked &&
+      !checkboxes[2].checked
+    ) {
+      args = {
+        length: 15,
+        characters: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz",
+      };
+    }
+
+    // Only numbers
+    if (
+      !checkboxes[0].checked &&
+      checkboxes[1].checked &&
+      !checkboxes[2].checked
+    ) {
+      args = {
+        length: 15,
+        type: "numeric",
+      };
+    }
+
+    // Only symbols
+    if (
+      !checkboxes[0].checked &&
+      !checkboxes[1].checked &&
+      checkboxes[2].checked
+    ) {
+      args = {
+        length: 15,
+        characters: "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~",
+      };
+    }
+
+    // Letters + numbers
+    if (
+      checkboxes[0].checked &&
+      checkboxes[1].checked &&
+      !checkboxes[2].checked
+    ) {
+      args = {
+        length: 15,
+        type: "alphanumeric",
+      };
+    }
+
+    // Letters + symbols
+    if (
+      !checkboxes[0].checked &&
+      checkboxes[1].checked &&
+      checkboxes[2].checked
+    ) {
+      args = {
+        length: 15,
+        characters:
+          "!\"#$%&'()*+,-./:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~",
+      };
+    }
+
+    // Numbers + symbols
+    if (
+      checkboxes[0].checked &&
+      !checkboxes[1].checked &&
+      checkboxes[2].checked
+    ) {
+      args = {
+        length: 15,
+        characters: "!\"#$%&'()*+,-./0123456789:;<=>?@[\\]^_`{|}~",
+      };
+    }
+
+    // Letters + numbers + symbols
+    if (
+      checkboxes[0].checked &&
+      checkboxes[1].checked &&
+      checkboxes[2].checked
+    ) {
+      args = {
+        length: 15,
+        type: "ascii-printable",
+      };
+    }
+
+    if (args) {
+      password = cryptoRandomString(args);
+    }
     this.setState({
       password: password,
     });
@@ -41,22 +128,29 @@ class App extends Component {
     this.generatePassword();
   };
 
-  onCheckboxChanged = (index) => {
-    this.setState((prevState) => {
-      const newCheckboxes = [...prevState.checkboxes];
-      newCheckboxes[index] = { ...prevState.checkboxes[index] };
-      newCheckboxes[index].checked = !prevState.checkboxes[index].checked;
-      return {
-        checkboxes: newCheckboxes,
-      };
+  onCheckboxChanged = (event, index) => {
+    const newCheckboxes = [...this.state.checkboxes];
+    newCheckboxes[index] = { ...this.state.checkboxes[index] };
+    newCheckboxes[index].checked = !this.state.checkboxes[index].checked;
+    this.setState({
+      checkboxes: newCheckboxes,
     });
+    this.generatePassword(newCheckboxes);
   };
 
   render() {
+    let password = "";
+    if (this.state.password) {
+      password = this.state.password;
+    } else {
+      password = (
+        <span className={classes.Red}>Select at least one option</span>
+      );
+    }
     return (
       <div className={classes.App}>
         <h1>Generate secure password</h1>
-        <p className={classes.Password}>{this.state.password}</p>
+        <p className={classes.Password}>{password}</p>
         <form onSubmit={this.onFormSubmit}>
           {this.state.checkboxes.map((checkbox, index) => (
             <Checkbox
@@ -64,15 +158,13 @@ class App extends Component {
               value={checkbox.value}
               label={checkbox.label}
               checked={checkbox.checked}
-              changed={() => this.onCheckboxChanged(index)}
+              changed={(event) => this.onCheckboxChanged(event, index)}
             />
           ))}
           <Button type="button" btnType="link" clicked={this.copyToClipboard}>
             <FontAwesomeIcon icon={faCopy} /> Copy to clipboard
           </Button>
-          <Button type="submit" clicked={this.generatePassword}>
-            Generate
-          </Button>
+          <Button type="submit">Generate</Button>
         </form>
       </div>
     );
